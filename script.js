5cb46928f3b658c286866b383278467095871a05
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-12-06 13:31:41 +0000

    Implement clipboard copy for generated password

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const randomFunc = {
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
+
+clipboard.onclick = () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return alert("No result to copy!");
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard!");
+};
