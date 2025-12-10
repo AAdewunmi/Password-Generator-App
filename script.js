f07825ad915cb8368c501232cf35a423cbc338cb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-12-10 04:04:01 +0000

    Implement getRandomNumber() function

--- script.js
+++ script.js
@@ -110,9 +110,9 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-// function getRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
 
 // function getRandomSymbol() {
 //   const symbols = "!@#$%^&*(){}[]=<>/,.";
