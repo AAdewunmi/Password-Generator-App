1c18045da98c600b9959ea190eb38eb1368de8d6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-12-10 04:03:22 +0000

    Implement getRandomUpper() function

--- script.js
+++ script.js
@@ -106,9 +106,9 @@ function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// function getRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
 // function getRandomNumber() {
 //   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
